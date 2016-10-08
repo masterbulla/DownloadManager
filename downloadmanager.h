564babb5031cf124cf264a929ed6f131aa8a40a2
Syclopse <syclopse@ymail.com>
2016-10-08 08:05:20 -0400

    Initial commit

--- downloadmanager.h
+++ downloadmanager.h
@@ -3,15 +3,44 @@
 
 #include <QObject>
 
+#include <QFile>
+#include <QObject>
+#include <QQueue>
+#include <QTime>
+#include <QUrl>
+#include <QNetworkAccessManager>
+
+#include "textprogressbar.h"
+
 class DownloadManager : public QObject
 {
     Q_OBJECT
 public:
     explicit DownloadManager(QObject *parent = 0);
 
-signals:
+    void append(const QUrl &url);
+    void append(const QStringList &urlList);
+    QString saveFileName(const QUrl &url);
 
-public slots:
+signals:
+    void finished();
+
+private slots:
+    void startNextDownload();
+    void downloadProgress(qint64 bytesReceived, qint64 bytesTotal);
+    void downloadFinished();
+    void downloadReadyRead();
+
+private:
+    QNetworkAccessManager manager;
+    QQueue<QUrl> downloadQueue;
+    QNetworkReply *currentDownload;
+    QFile output;
+    QTime downloadTime;
+    TextProgressBar progressBar;
+
+    int downloadedCount;
+    int totalCount;
 };
 
 #endif // DOWNLOADMANAGER_H
